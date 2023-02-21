e04ec9d6682265fc86933e143a8866e97627be69
qtr202 <qatarna99@gmail.com>
2023-02-21 10:11:16 +0300

    Abdullah 8708

---
+++ Abdullah 8708.go
@@ -0,0 +1,19 @@
+package math
+
+import "testing"
+
+func TestAdd(t *testing.T){
+
+    got := Add(4, 6)
+    want := 10
+
+    if got != want {
+        t.Errorf("got %q, wanted %q", got, want)
+    }
+}
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+          
